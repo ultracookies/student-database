52d82260db7f9cdbee86d0a305fa4539309d24d4
Ismail Ajaz <ismail.ajaz19@gmail.com>
2024-07-30 00:07:07 -0400

    initial commit

---
+++ src/main/java/com/sdb/db/StudentDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.sdb.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDatabaseApplication.class, args);
+	}
+
+}
